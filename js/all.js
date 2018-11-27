80606be08fa1257eec6ee343e1f0b6fdcbbb9016
Ray <loveyou520babytw@gmail.com>
2018-11-27 12:25:24 +0800

    firet

---
+++ js/all.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    urlAPI: 'https://api.github.com/users/hsiangfeng/repos',
+    SortAPI: '?sort=updated',
+    repo: []
+  },
+  computed: {
+    getRepo: function () {
+      var xhr = new XMLHttpRequest();
+      var self = this;
+      var innterUrl = this.urlAPI + this.SortAPI;
+      console.log(innterUrl);
+      xhr.open('GET', innterUrl);
+      xhr.onload = function () {
+        self.repo = JSON.parse(xhr.responseText);
+      };
+      xhr.send();
+    }
+  }
+})
