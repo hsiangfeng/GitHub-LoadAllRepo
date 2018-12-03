6d6480bb4a48b7b1fd1d702f3c5fd19b3b4258d4
Ray <loveyou520babytw@gmail.com>
2018-12-03 15:59:19 +0800

    新增功能修正圖片
    1.新增按鈕查詢數量
    2.圖片修正改用github

--- js/all.js
+++ js/all.js
@@ -3,13 +3,14 @@ var app = new Vue({
   data: {
     urlAPI: 'https://api.github.com/users/hsiangfeng/repos',
     SortAPI: '?sort=updated',
-    repo: []
+    repo: [],
+    searchPage='',
   },
   computed: {
     getRepo: function () {
       var xhr = new XMLHttpRequest();
       var self = this;
-      var innterUrl = this.urlAPI + this.SortAPI;
+      var innterUrl = this.urlAPI + this.SortAPI + this.searchPage;
       console.log(innterUrl);
       xhr.open('GET', innterUrl);
       xhr.onload = function () {
