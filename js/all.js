e1e2f47a5a26b6ba5fc5b39566adb3c2d774fa06
Ray <loveyou520babytw@gmail.com>
2018-12-08 14:56:22 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -7,12 +7,13 @@ var app = new Vue({
     sortApi: '?sort=updated',
     repo: [],
     searchPage: '&per_page=10&sha=',
+    clientId: '&Client_ID="6b4879b5d928051d0dd5"'
   },
   computed: {
     getRepo: function () {
       var xhr = new XMLHttpRequest();
       var vm = this;
-      var innterUrl = this.urlApi + this.urlName + this.typeApi + this.sortApi + this.searchPage;
+      var innterUrl = this.urlApi + this.urlName + this.typeApi + this.sortApi + this.searchPage + this.clientId;
       xhr.open('GET', innterUrl);
       xhr.send(null);
       xhr.onload = function () {
