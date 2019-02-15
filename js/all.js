34527f431125036ab6017ad5072dc849908b00bd
Ray <loveyou520babytw@gmail.com>
2019-02-15 12:26:18 +0800

    修正小問題

--- js/all.js
+++ js/all.js
@@ -19,7 +19,7 @@ const app = new Vue({
           this.urlAll = _Url
           this.repo = item
         }).catch(error => {
-          console.log(error)
+          window.alert('目前出現錯誤：' + error)
         })
     }
   }
