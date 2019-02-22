669c97758b87155ef30c5865910fb2e4c76f43c3
Ray <loveyou520babytw@gmail.com>
2019-02-22 08:52:50 +0800

    增加關注功能

--- js/all.js
+++ js/all.js
@@ -3,29 +3,46 @@ const app = new Vue({
   data: {
     urlApi: 'https://api.github.com/users/',
     typeApi: '/repos',
-    urlName: 'vuejs',
+    urlName: '',
     sortApi: '?sort=updated',
     repo: [],
     searchPage: '&per_page=5&sha=',
     urlAll: ''
   },
-  computed: {
-    getRepo: function () {
+  methods: {
+    initRepo () {
+      this.urlName = JSON.parse(window.localStorage.getItem('githubName')) || ''
       let _Url = this.urlApi + this.urlName + this.typeApi + this.sortApi + this.searchPage
-      window.fetch(_Url, { method: 'get' })
+      this.ajaxRepo(_Url)
+    },
+    getRepo () {
+      let _Url = this.urlApi + this.urlName + this.typeApi + this.sortApi + this.searchPage
+      this.ajaxRepo(_Url)
+    },
+    ajaxRepo (url) {
+      const vm = this
+      window.fetch(url, { method: 'get' })
         .then(data => {
           return data.json()
         }).then(item => {
           if (item.documentation_url === 'https://developer.github.com/v3/#rate-limiting') {
             window.alert('出現錯誤：' + item.message)
           } else {
-            this.urlAll = _Url
-            this.repo = item
+            vm.urlAll = url
+            vm.repo = item
           }
         }).catch(error => {
           window.alert('目前出現錯誤：' + error)
         })
+    },
+    addRepo () {
+      const vm = this
+      window.localStorage.setItem('githubName', JSON.stringify(vm.urlName))
+      window.alret(`已將目前 ${vm.urlName} 對象加入關注。`)
     }
+  },
+  created () {
+    this.initRepo()
   }
 })
 window.onload = function () {
