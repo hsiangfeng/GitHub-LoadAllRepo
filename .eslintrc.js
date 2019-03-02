6f1741f813a5dce758150d44bf309c04d2742a0e
Ray <loveyou520babytw@gmail.com>
2019-03-03 00:02:10 +0800

    調整ESLINT

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
-    "extends": "standard",
-    "env": {
-        "jquery": true
-      }
+  "env": {
+    "browser": true,
+    "es6": true,
+    "jquery": true
+  },
+  "extends": "airbnb-base",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "comma-dangle": [
+      "error",
+      "never"
+    ], // 該規則關閉強制使用對象和數組文字中的逗號
+    "no-console": 0, // 關掉console.log()提示
+    "no-alert": 0, // alert提示
+  }
 };
--- js/all.js
+++ js/all.js
@@ -1,3 +1,4 @@
+/* global Vue */
 const app = new Vue({
   el: '#app',
   data: {
@@ -10,40 +11,40 @@ const app = new Vue({
     urlAll: ''
   },
   methods: {
-    initRepo () {
-      let localUserName = JSON.parse(window.localStorage.getItem('githubName')) || []
+    initRepo() {
+      const localUserName = JSON.parse(window.localStorage.getItem('githubName')) || [];
       if (localUserName.length !== 0) {
-        this.urlName = localUserName
+        this.urlName = localUserName;
       }
-      this.ajaxRepo()
+      this.ajaxRepo();
     },
-    ajaxRepo () {
-      const vm = this
-      let _Url = this.urlApi + this.urlName + this.typeApi + this.sortApi + this.searchPage
-      window.fetch(_Url, { method: 'get' })
-        .then(data => {
-          return data.json()
-        }).then(item => {
+    ajaxRepo() {
+      const vm = this;
+      const Url = this.urlApi + this.urlName + this.typeApi + this.sortApi + this.searchPage;
+      window.fetch(Url, { method: 'get' })
+        .then((data) => {
+          return data.json();
+        }).then((item) => {
           if (item.documentation_url === 'https://developer.github.com/v3/#rate-limiting') {
-            window.alert('出現錯誤：' + item.message)
+            window.alert(`出現錯誤：${item.message}`);
           } else {
-            vm.urlAll = _Url
-            vm.repo = item
+            vm.urlAll = Url;
+            vm.repo = item;
           }
-        }).catch(error => {
-          window.alert('目前出現錯誤：' + error)
-        })
+        }).catch((error) => {
+          window.alert(`目前出現錯誤：${error}`);
+        });
     },
-    addRepo () {
-      const vm = this
-      window.localStorage.setItem('githubName', JSON.stringify(vm.urlName))
-      window.alert(`已將目前 ${vm.urlName} 對象加入關注。`)
+    addRepo() {
+      const vm = this;
+      window.localStorage.setItem('githubName', JSON.stringify(vm.urlName));
+      window.alert(`已將目前 ${vm.urlName} 對象加入關注。`);
     }
   },
-  created () {
-    this.initRepo()
+  created() {
+    this.initRepo();
   }
-})
-window.onload = function () {
-  $('.loading').fadeOut(1500)
-}
+});
+window.onload = () => {
+  $('.loading').fadeOut(1500);
+};
